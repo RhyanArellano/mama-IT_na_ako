76984b29eba1cf245dc701f6c1133b53029173c9
RhyanArellano <rhyan.arellano@students.isatu.edu.ph>
2026-09-16 07:14:35 +0000

    commit4

---
+++ newfoldr/commit4.R
@@ -0,0 +1,16 @@
+
+joseAge <- 10
+juanAge <- 20
+
+addAge = joseAge + juanAge
+addAge = 
+  
+mulAge = joseAge * juanAge
+mulAge =
+  
+subAge = joseAge - juanAge
+subAge =
+  
+divAge = joseAge / juanAge
+divAge =
+  
